cfce64a1c17a3d0768f584e5f700aed9dbbe8576
heabeounMTKO <heabeoun69zin@gmail.com>
2022-05-12 00:36:42 +0700

    Update nem.py

--- nem.py
+++ nem.py
@@ -4,34 +4,60 @@ from pathlib import Path
 from os.path import join
 import subprocess
 
-print("welcome to nem, lazy tool for lazy people by lazy people")
-currentDir = input("WHERE IS THE TEXTURE (path pls)\n")
-subfoldersList = os.listdir(currentDir)
-autoMatPath = os.path.join(os.getcwd() + "\matauto.py")
+print("""\
+          _____                    _____                    _____          
+         /\    \                  /\    \                  /\    \         
+        /::\____\                /::\    \                /::\____\        
+       /::::|   |               /::::\    \              /::::|   |        
+      /:::::|   |              /::::::\    \            /:::::|   |        
+     /::::::|   |             /:::/\:::\    \          /::::::|   |        
+    /:::/|::|   |            /:::/__\:::\    \        /:::/|::|   |        
+   /:::/ |::|   |           /::::\   \:::\    \      /:::/ |::|   |        
+  /:::/  |::|   | _____    /::::::\   \:::\    \    /:::/  |::|___|______  
+ /:::/   |::|   |/\    \  /:::/\:::\   \:::\    \  /:::/   |::::::::\    \ 
+/:: /    |::|   /::\____\/:::/__\:::\   \:::\____\/:::/    |:::::::::\____\
+\::/    /|::|  /:::/    /\:::\   \:::\   \::/    /\::/    / ~~~~~/:::/    /
+ \/____/ |::| /:::/    /  \:::\   \:::\   \/____/  \/____/      /:::/    / 
+         |::|/:::/    /    \:::\   \:::\    \                  /:::/    /  
+         |::::::/    /      \:::\   \:::\____\                /:::/    /   
+         |:::::/    /        \:::\   \::/    /               /:::/    /    
+         |::::/    /          \:::\   \/____/               /:::/    /     
+         /:::/    /            \:::\    \                  /:::/    /      
+        /:::/    /              \:::\____\                /:::/    /       
+        \::/    /                \::/    /                \::/    /        
+         \/____/                  \/____/                  \/____/         
+                                                                           
+""")
+print("       THE BLENDER TOOLKIT FOR LAZY PEOPLE BY LAZY PEOPLE          ")
 
-def createBlendInSubfolders():
-    
-    for subFolders in subfoldersList:
-        
-        texPath = Path(currentDir) / Path(subFolders)
-        
-        texName = texPath.stem + ".blend"
-        
-        createBlendFile = open(os.path.join(texPath,texName), 'w')
-        print('created blend file: ' + createBlendFile.name)
+class materialFromPath:
+    currentDir = input("enter your command\n")
+    subfoldersList = os.listdir(currentDir)
+    autoMatPath = os.path.join(os.getcwd() + "\matauto.py")
 
-def createMaterialInBlend():
-    for blendFiles in subfoldersList:
+    def createBlendInSubfolders():
         
-        blendPath = Path(currentDir) / Path(blendFiles)
-        
-        blendName = blendPath.stem + '.blend'
-        blendFilePath = os.path.join(blendPath,blendName)
-        print("opening file: " + blendName)
-        launchBlender = subprocess.run(["blender","--enable-autoexec",blendFilePath, '--python', autoMatPath])
-        #DEVNULL HIDES BLENDER CONSOLE OUTPUT
-        print('file rewritten: ' + blendFilePath)
-        print('material created: ' + blendName)
+        for subFolders in subfoldersList:
+            
+            texPath = Path(currentDir) / Path(subFolders)
+            
+            texName = texPath.stem + ".blend"
+            
+            createBlendFile = open(os.path.join(texPath,texName), 'w')
+            print('created blend file: ' + createBlendFile.name)
+
+    def createMaterialInBlend():
+        for blendFiles in subfoldersList:
+            
+            blendPath = Path(currentDir) / Path(blendFiles)
+            
+            blendName = blendPath.stem + '.blend'
+            blendFilePath = os.path.join(blendPath,blendName)
+            print("opening file: " + blendName)
+            launchBlender = subprocess.run(["blender","--enable-autoexec",blendFilePath, '--python', autoMatPath])
+            #DEVNULL HIDES BLENDER CONSOLE OUTPUT
+            print('file rewritten: ' + blendFilePath)
+            print('material created: ' + blendName)
         
 
 createBlendInSubfolders()
