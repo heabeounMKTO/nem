6ed3689266c56895a573da27dbf23c142ae5bc54
heabeoun <heabeoun69zin@gmail.com>
2022-05-16 09:01:40 +0700

    fix file path

--- nem.py
+++ nem.py
@@ -2,7 +2,9 @@ import os
 import sys
 from pathlib import Path
 from os.path import join
+from os.path import exists
 import subprocess
+from venv import create
 
 print("""\
  
@@ -26,40 +28,49 @@ print(" THE BLENDER TOOLKIT FOR LAZY PEOPLE BY LAZY PEOPLE ")
 print(" this program is provided as is there are no guaratees ")
 
 
-currentDir = input("enter texture directory that contains all the subfolders with textures\n")
-subfoldersList = os.listdir(currentDir)
+
+mainTextureFolder = input("enter texture directory that contains all the subfolders with textures\n")
 autoMatPath = os.path.join(os.getcwd() + "\matauto.py")   
+texFolders = list()
 
 def createBlendInSubfolders():
         
-    for subFolders in subfoldersList:
+    for root,dirs,files in os.walk(mainTextureFolder):
             
-        texPath = Path(currentDir) / Path(subFolders)
-            
-        texName = texPath.stem + ".blend"
+        if not dirs:
+            texFolders.append(root)
+    print(texFolders)
+    for blendPath in texFolders:    
+        blendPath = Path(blendPath)
+        blendName = blendPath.stem + ".blend"
         
-        if(os.path.join(texPath,texName) == None):    
-            createBlendFile = open(os.path.join(texPath,texName), 'w')
+        checkForBlendFile = Path(os.path.join(blendPath,blendName))
+        
+        if(checkForBlendFile.is_file() == False):
+            createBlendFile = open(os.path.join(blendPath, blendName), 'w')
+            print("creating blend file: " + createBlendFile.name)
+            print("created blend files:" + createBlendFile.name)
         else:
-            return None
-        print('created blend file: ' + createBlendFile.name)
+            print("blend file " + checkForBlendFile.name + " already exists!")
 
-def createMaterialInBlend():
-    for blendFiles in subfoldersList:
+           
+    
+
+    
+# def createMaterialInBlend():
+#     for blendFiles in mainTexTureFolder:
             
-        blendPath = Path(currentDir) / Path(blendFiles)
+#         blendPath = Path(currentDir) / Path(blendFiles)
             
-        blendName = blendPath.stem + '.blend'
-        blendFilePath = os.path.join(blendPath,blendName)
-        print("opening file: " + blendName)
-        launchBlender = subprocess.run(["blender","--enable-autoexec",blendFilePath, '--python', autoMatPath])
-        #DEVNULL HIDES BLENDER CONSOLE OUTPUT
-        print('file rewritten: ' + blendFilePath)
-        print('material created: ' + blendName)
+#         blendName = blendPath.stem + '.blend'
+#         blendFilePath = os.path.join(blendPath,blendName)
+#         print("opening file: " + blendName)
+#         launchBlender = subprocess.run(["blender","--enable-autoexec",blendFilePath, '--python', autoMatPath])
+#         #DEVNULL HIDES BLENDER CONSOLE OUTPUT
+#         print('file rewritten: ' + blendFilePath)
+#         print('material created: ' + blendName)
         
-
 createBlendInSubfolders()
-createMaterialInBlend()    
     
     
 
